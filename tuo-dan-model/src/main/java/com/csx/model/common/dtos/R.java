5b458e4c58477dcbd99732df29b52f5e1296fad3
czx <1924086038@qq.com>
2023-11-25 22:34:38 +0800

    init project

--- tuo-dan-model/src/main/java/com/csx/model/common/dtos/R.java
+++ tuo-dan-model/src/main/java/com/csx/model/common/dtos/R.java
@@ -159,7 +159,7 @@ public class R<T> implements Serializable {
         PageResponseResult responseResult = new PageResponseResult(1,5,50);
         List list = new ArrayList();
         list.add("itcast");
-        list.add("itheima");
+        list.add("csx");
         responseResult.setData(list);
         System.out.println(JSON.toJSONString(responseResult));
 
